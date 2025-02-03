d78158495b4ce0a5c54e4c704e68fa096feca3d1
TheVerybest098 <231094@student.wqe.ac.uk>
2025-02-03 21:48:43 +0000

    Added more validation

--- Register.php
+++ Register.php
@@ -1,36 +1,35 @@
 <?php
+$con = mysqli_connect('localhost', 'root', 'root', 'unityaccess');
 
-	$con = mysqli_connect('localhost', 'root', 'root', 'unityaccess');
+if (mysqli_connect_errno()) {
+    echo "1: Connection failed"; 
+    exit();
+}
 
-	// Check that connection happened
-	if (mysqli_connect_errno())
-	{
-    	echo "1: Connection failed"; // Error code #1 = connection failed
-    	exit();
-	}
+$username = $_POST["name"];
+$password = $_POST["password"];
 
-	$username = $_POST["name"];
-	$password = $_POST["password"];
+// Check if name exists
+$namecheckquery = "SELECT username FROM players WHERE username='" . $username . "';";
+$namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed");
 
-	// Check if name exists
-	$namecheckquery = "SELECT username FROM players WHERE username='" . $username . "';";
+if (mysqli_num_rows($namecheck) > 0) {
+    echo "3: Name already exists";
+    exit();
+}
 
-	$namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed"); // Error code #2 - name check query failed
+// Validate password in PHP
+if (strlen($password) < 8 || !preg_match('/[A-Z]/', $password) || !preg_match('/[0-9]/', $password) || !preg_match('/[\W_]/', $password)) {
+    echo "7: Password does not meet requirements";  // New error code
+    exit();
+}
 
-	if (mysqli_num_rows($namecheck) > 0)
-	{
-    	echo "3: Name already exists"; // Error code #3 - name exists cannot register
-    	exit();
-	}
+// Add user to the table
+$salt = "\$5\$rounds=5000\$" . "steamedhams" . $username . "\$";
+$hash = crypt($password, $salt);
+$insertuserquery = "INSERT INTO players (username, hash, salt) VALUES ('$username', '$hash', '$salt');";
 
-	// Add user to the table
-	$salt = "\$5\$rounds=5000\$" . "steamedhams" . $username . "\$";
-	$hash = crypt($password, $salt);
-	$insertuserquery = "INSERT INTO players (username, hash, salt) VALUES ('" . $username . "', '" . $hash . "', '" . $salt . "');";
-	mysqli_query($con, $insertuserquery) or die("4: Insert player query failed"); // Error code #4 - insert query failed
+mysqli_query($con, $insertuserquery) or die("4: Insert player query failed");
 
-	echo("0");
-
-
-
+echo "0";
 ?>
